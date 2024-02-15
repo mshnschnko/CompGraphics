1bbb9647ffc0252f52ca51371b3ef71ab8881ef2
Mikhail Anishchenko <misha321654897i@gmail.com>
2024-02-15 13:23:22 +0300

    renamed lab1 to lab and did lab2

--- lab/Resource.h
+++ lab/Resource.h
@@ -1,17 +1,17 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Включаемый файл, созданный в Microsoft Visual C++.
-// Используется lab1.rc
+// Используется lab.rc
 
 #define IDS_APP_TITLE			103
 
 #define IDR_MAINFRAME			128
-#define IDD_LAB1_DIALOG	102
+#define IDD_LAB_DIALOG	102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_LAB1			107
+#define IDI_LAB			107
 #define IDI_SMALL				108
-#define IDC_LAB1			109
+#define IDC_LAB			109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
